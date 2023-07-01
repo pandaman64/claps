7852c531417d986e1d628e9518fbb2e52bd6dd4c
pandaman64 <kointosudesuyo@infoseek.jp>
2023-07-01 19:58:12 +0900

    Implement an interpreter

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,13 @@ mod grammar {
             _close_paren: (),
             #[rust_sitter::leaf(text = "{")]
             _open_body: (),
-            #[rust_sitter::delimited(#[rust_sitter::leaf(text = ";")] ())]
-            body: Vec<Expr>,
+            body: Expr,
             #[rust_sitter::leaf(text = "}")]
             _close_body: (),
-        }
+        },
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum Expr {
         Var(Ident),
         Number {
@@ -48,6 +47,7 @@ mod grammar {
             _op: (),
             right: Box<Expr>,
         },
+        #[rust_sitter::prec(3)]
         Call {
             fun: Box<Expr>,
             #[rust_sitter::leaf(text = "(")]
@@ -57,9 +57,20 @@ mod grammar {
             #[rust_sitter::leaf(text = ")")]
             _close_paren: (),
         },
+        Let {
+            #[rust_sitter::leaf(text = "let")]
+            _let: (),
+            name: Ident,
+            #[rust_sitter::leaf(text = "=")]
+            _eq: (),
+            value: Box<Expr>,
+            #[rust_sitter::leaf(text = ";")]
+            _semicolon: (),
+            body: Box<Expr>,
+        },
     }
-    
-    #[derive(Debug)]
+
+    #[derive(Debug, Clone)]
     pub struct Ident {
         #[rust_sitter::leaf(pattern = "[a-zA-Z_][a-zA-Z0-9_]*", transform = str::to_string)]
         pub ident: String,
@@ -72,6 +83,111 @@ mod grammar {
     }
 }
 
+mod interp {
+    use std::{collections::HashMap, rc::Rc};
+
+    use crate::grammar::*;
+
+    #[derive(Debug)]
+    pub struct Env<'p> {
+        pub parent: Option<&'p Env<'p>>,
+        pub vars: HashMap<String, Value>,
+    }
+
+    impl Env<'_> {
+        fn lookup(&self, var: &str) -> Option<&Value> {
+            self.vars.get(var).or_else(|| self.parent?.lookup(var))
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Value {
+        Number(i64),
+        Fun { params: Vec<String>, body: Rc<Expr> },
+    }
+
+    pub fn run(env: &Env, expr: &Expr) -> Value {
+        match expr {
+            Expr::Var(var) => env
+                .lookup(&var.ident)
+                .unwrap_or_else(|| panic!("variable {} not found", var.ident))
+                .clone(),
+            Expr::Number { value } => Value::Number(*value),
+            Expr::Add { left, _op, right } => {
+                let left = run(env, left);
+                let right = run(env, right);
+                match (left, right) {
+                    (Value::Number(left), Value::Number(right)) => Value::Number(left + right),
+                    _ => panic!("type error"),
+                }
+            }
+            Expr::Mul { left, _op, right } => {
+                let left = run(env, left);
+                let right = run(env, right);
+                match (left, right) {
+                    (Value::Number(left), Value::Number(right)) => Value::Number(left * right),
+                    _ => panic!("type error"),
+                }
+            }
+            Expr::Call {
+                fun,
+                args,
+                ..
+            } => {
+                let fun = run(env, fun);
+                let args = args.iter().map(|arg| run(env, arg)).collect::<Vec<_>>();
+                match fun {
+                    Value::Fun { params, body } => {
+                        assert!(params.len() == args.len());
+                        let new_env = Env {
+                            parent: Some(env),
+                            vars: params.into_iter().zip(args).collect(),
+                        };
+                        run(&new_env, &body)
+                    }
+                    _ => panic!("type error"),
+                }
+            }
+            Expr::Let {
+                name,
+                value,
+                body,
+                ..
+            } => {
+                let value = run(env, value);
+                let new_env = Env {
+                    parent: Some(env),
+                    vars: HashMap::from([(name.ident.clone(), value)]),
+                };
+                run(&new_env, body)
+            }
+        }
+    }
+}
+
+use std::{collections::HashMap, rc::Rc};
+
 fn main() {
-    println!("{:?}", grammar::parse("fun main() { 1 + 2 * 3; f(v) }"));
+    let program = grammar::parse("fun main() { let x = 1 + 2 * 3; x }").unwrap();
+    // println!("{:#?}", program);
+    let env = interp::Env {
+        parent: None,
+        // bind each top-level function to its function name
+        vars: HashMap::from_iter(program.definitions.iter().map(|def| match def {
+            grammar::Definition::Fun { name, args, body, .. } => (
+                name.ident.clone(),
+                interp::Value::Fun {
+                    params: args.iter().map(|arg| arg.ident.clone()).collect(),
+                    body: Rc::new(body.clone()),
+                },
+            ),
+        })),
+    };
+    let result = interp::run(&env, &grammar::Expr::Call {
+        fun: Box::new(grammar::Expr::Var(grammar::Ident { ident: "main".to_string() })),
+        args: vec![],
+        _open_paren: (),
+        _close_paren: (),
+    });
+    println!("{:?}", result);
 }
